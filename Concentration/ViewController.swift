22a55a423e38ae99aca7e18c9722598c392244d5
Ivan <videnov.ivan@gmail.com>
2020-09-14 11:28:50 +0300

    init

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Ivan Videnov on 14.09.20.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
